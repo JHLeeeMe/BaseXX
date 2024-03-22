ce80dc23265a52313f94d7b73a84d81398439ade
JHLeeeMe <lejung92@gmail.com>
2024-03-22 15:35:13 +0900

    [Update] BaseXX table
    add base64, base64 urlsafe, base32, base32 hex, base16 table

--- include/BaseXX.h
+++ include/BaseXX.h
@@ -1,8 +1,68 @@
+/// rfc4648
+/// https://datatracker.ietf.org/doc/html/rfc4648
+///
+
 #ifndef BASEXX_H
 #define BASEXX_H
 
 
 #include <iostream>
+
+/// The Base 64 Alphabet Table
+/// https://datatracker.ietf.org/doc/html/rfc4648#section-4
+///
+static const unsigned char base64_table[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  // 0 ~ 7
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',  // 8 ~ 15
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',  // 16 ~ 23
+    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',  // 24 ~ 31
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',  // 32 ~ 39
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',  // 40 ~ 47
+    'w', 'x', 'y', 'z', '0', '1', '2', '3',  // 48 ~ 55
+    '4', '5', '6', '7', '8', '9', '+', '/',  // 56 ~ 63
+};
+
+/// The Base 64 Alphabet Table (URL and Filename safe)
+/// https://datatracker.ietf.org/doc/html/rfc4648#section-5
+///
+static const unsigned char base64_urlsafe_table[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  // 0 ~ 7
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',  // 8 ~ 15
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',  // 16 ~ 23
+    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',  // 24 ~ 31
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',  // 32 ~ 39
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',  // 40 ~ 47
+    'w', 'x', 'y', 'z', '0', '1', '2', '3',  // 48 ~ 55
+    '4', '5', '6', '7', '8', '9', '-', '_',  // 56 ~ 63
+};
+
+/// The Base 32 Alphabet Table
+/// https://datatracker.ietf.org/doc/html/rfc4648#section-6
+///
+static const unsigned char base32_table[32] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  // 0 ~ 7
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',  // 8 ~ 15
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',  // 16 ~ 23
+    'Y', 'Z', '2', '3', '4', '5', '6', '7',  // 24 ~ 31
+};
+
+/// The Base 32 Alphabet Table (Extended Hex)
+/// https://datatracker.ietf.org/doc/html/rfc4648#section-7
+///
+static const unsigned char base32_hex_table[32] = {
+    '0', '1', '2', '3', '4', '5', '6', '7',  // 0 ~ 7
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',  // 8 ~ 15
+    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',  // 16 ~ 23
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',  // 24 ~ 31
+};
+
+/// The Base 16 Alphabet Table
+/// https://datatracker.ietf.org/doc/html/rfc4648#section-8
+///
+static const unsigned char base16_table[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7',  // 0 ~ 7
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',  // 8 ~ 15
+};
 
 namespace BaseXX
 {
@@ -33,3 +93,4 @@ namespace _16_
 
 
 #endif  // BASEXX_H
+
