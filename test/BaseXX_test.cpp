0669bd1c73289217cba0242756120dc78092a9b0
JHLeeeMe <lejung92@gmail.com>
2024-03-24 15:33:37 +0900

    [Update] base32 functions
    modify `unsigned char` to `uint8_t`
    add base32 encode functions
    add base32 test case

--- test/BaseXX_test.cpp
+++ test/BaseXX_test.cpp
@@ -1,4 +1,5 @@
 #include <array>
+
 #include "gtest/gtest.h"
 
 #include "BaseXX.h"
@@ -17,19 +18,19 @@ TEST(BaseXX, _64_)
     ASSERT_EQ("YA==", base64::encode("`"));
     ASSERT_EQ("7ZWc6riA", base64::encode("한글"));
 
-    {  // initializer_list<unsigned char>
+    {  // initializer_list<uint8_t>
         ASSERT_EQ("7ZWc", base64::encode({ 0xed, 0x95, 0x9c }));  // '한'
         ASSERT_EQ("IA==", base64::encode({ ' ', }));
     }
 
-    {  // std::array<unsigned char, N>
-        std::array<unsigned char, 0> arr_empty{};
+    {  // std::array<uint8_t, N>
+        std::array<uint8_t, 0> arr_empty{};
         ASSERT_EQ("", base64::encode(arr_empty));
 
-        std::array<unsigned char, 3> arr_3{ 0xed, 0x95, 0x9c };  // '한'
+        std::array<uint8_t, 3> arr_3{ 0xed, 0x95, 0x9c };  // '한'
         ASSERT_EQ("7ZWc", base64::encode(arr_3));
 
-        std::array<unsigned char, 2> arr_2{ 'a', 'A' };  // "aA"
+        std::array<uint8_t, 2> arr_2{ 'a', 'A' };  // "aA"
         ASSERT_EQ("YUE=", base64::encode(arr_2));
     }
     ASSERT_EQ("////", base64::encode({ 0xff, 0xff, 0xff }));
@@ -38,8 +39,32 @@ TEST(BaseXX, _64_)
 
 TEST(BaseXX, _32_)
 {
-    ::BaseXX::_32_::hello();
-    base32::hello();
+    ASSERT_EQ("", base32::encode(""));
+    ASSERT_EQ("", base32::encode(std::string()));
+    ASSERT_EQ("", base32::encode({}));
+
+    ASSERT_EQ("LQ======", base32::encode("\\"));
+    ASSERT_EQ("LRXA====", base32::encode("\\n"));
+    ASSERT_EQ("LRXFYMA=", base32::encode("\\n\\0"));
+    ASSERT_EQ("EA======", base32::encode(" "));
+    ASSERT_EQ("MA======", base32::encode("`"));
+    ASSERT_EQ("5WKZZ2VYQA======", base32::encode("한글"));
+
+    {  // initializer_list<uint8_t>
+        ASSERT_EQ("5WKZY===", base32::encode({ 0xed, 0x95, 0x9c }));  // '한'
+        ASSERT_EQ("EA======", base32::encode({ ' ', }));
+    }
+
+    {  // std::array<uint8_t, N>
+        std::array<uint8_t, 0> arr_empty{};
+        ASSERT_EQ("", base32::encode(arr_empty));
+
+        std::array<uint8_t, 3> arr_3{ 0xed, 0x95, 0x9c };  // '한'
+        ASSERT_EQ("5WKZY===", base32::encode(arr_3));
+
+        std::array<uint8_t, 2> arr_2{ 'a', 'A' };  // "aA"
+        ASSERT_EQ("MFAQ====", base32::encode(arr_2));
+    }
 }
 
 TEST(BaseXX, _16_)
